2981ce30081f3d371845da6cb56db87adedd56d7
thusithanjana <thusithanjana@gmail.com>
2023-03-02 22:59:00 +0530

    basic styles

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StylesComponent from './components/StylesComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StylesComponent/>
   );
 }
 
